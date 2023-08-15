31173d0ca856b8c6c679556cbc442a1d668c0de0
Luis david <david12.29@hotmail.com>
2023-08-15 14:16:32 -0500

    Update Navbar.js is now better

--- src/layout/Navbar.js
+++ src/layout/Navbar.js
@@ -3,7 +3,15 @@ import React from 'react'
 export default function Navbar() {
   return (
     <div>
-        
+        <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
+        <div className="container-fluid">
+        <a className="navbar-brand" href="#">Full Stack Application about dogs</a>
+        {/* <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span className="navbar-toggler-icon"></span>
+        </button> */}
+        <button className="btn btn-outline-light">Add Dog</button>
+        </div>
+        </nav>
 
     </div>
   )
